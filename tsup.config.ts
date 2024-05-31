0013f5895e378d5477668d9c3b53e6bf6d913be9
innovative-fn <martyniak.innovative@gmail.com>
2024-05-31 21:25:13 +0200

    chore: change build step

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  entry: ["src/index.ts", "src/core/index.ts", "src/common/index.ts"],
+  format: ["esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
   sourcemap: true,
   clean: true,
 });
