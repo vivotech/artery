36d9a3d48d898c831a966428f619bb9d6cb56c60
innovative-fn <martyniak.innovative@gmail.com>
2024-06-20 00:34:01 +0200

    refactor: pipe bash

--- src/common/bash.ts
+++ src/common/bash.ts
@@ -1,15 +1,19 @@
 import { spawn } from "child_process";
 import { EventEmitter } from "events";
 
+export interface BashOptions {
+  user?: string;
+  cwd?: string;
+}
+
 export async function bashAsync(
-  command: string,
   args: string[] = [],
-  options: { cwd?: string; user?: string } = {}
+  options: BashOptions = {}
 ) {
   return new Promise((res, rej) => {
     let response = "";
 
-    bash(command, args, options)
+    bash(args[0], args.slice(1), options)
       .on("data", (data) => (response += data))
       .on("error", (error) => rej(error))
       .on("close", (code) => res(response));
@@ -19,31 +23,26 @@ export async function bashAsync(
 export function bash(
   command: string,
   args: string[],
-  { cwd, user }: { cwd?: string; user?: string } = {}
+  { cwd, user }: BashOptions = {}
 ) {
   const emitter = new EventEmitter();
 
   try {
     const bash = spawn("sudo", ["-u", user ?? "dev", command, ...args], {
+      stdio: "pipe",
       shell: true,
       cwd,
     });
 
-    bash.stdout.on("data", (data) => {
-      emitter.emit("data", data.toString().trim());
-    });
+    bash.stdout.on("data", (data) =>
+      emitter.emit("data", data.toString().trim())
+    );
 
-    bash.stderr.on("data", (data) => {
-      emitter.emit("error", data.toString().trim());
-    });
-
-    bash.on("error", (error) => {
-      emitter.emit("error", error);
-    });
-
-    bash.on("close", (code) => {
-      emitter.emit("close", code);
-    });
+    bash.stderr.on("data", (data) =>
+      emitter.emit("error", data.toString().trim())
+    );
+    bash.on("error", (error) => emitter.emit("error", error));
+    bash.on("close", (code) => emitter.emit("close", code));
   } catch (error) {
     emitter.emit("error", error);
   }
