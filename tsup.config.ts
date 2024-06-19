b0d85abbeacd540ce5d6bfcab5bbc690907e12e6
innovative-fn <martyniak.innovative@gmail.com>
2024-06-19 23:03:47 +0200

    build: provide also esm build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/core/index.ts", "src/common/index.ts"],
+  entry: ["src/core/index.ts", "src/common/index.ts"],
   tsconfig: "./tsconfig.json",
-  format: ["cjs"],
+  format: ["cjs", "esm"],
   target: "esnext",
   clean: true,
   dts: true,
