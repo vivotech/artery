af729586d7c719af2a5772e59cf4c6a938636f37
innovative-fn <martyniak.innovative@gmail.com>
2024-06-19 23:01:37 +0200

    feat: async bash helper

--- src/common/bash.ts
+++ src/common/bash.ts
@@ -1,11 +1,33 @@
 import { spawn } from "child_process";
 import { EventEmitter } from "events";
 
-export function bash(command: string, args: string[]) {
+export async function bashAsync(
+  command: string,
+  args: string[] = [],
+  options: { cwd?: string; user?: string } = {}
+) {
+  return new Promise((res, rej) => {
+    let response = "";
+
+    bash(command, args, options)
+      .on("data", (data) => (response += data))
+      .on("error", (error) => rej(error))
+      .on("close", (code) => res(response));
+  });
+}
+
+export function bash(
+  command: string,
+  args: string[],
+  { cwd, user }: { cwd?: string; user?: string } = {}
+) {
   const emitter = new EventEmitter();
 
   try {
-    const bash = spawn(command, args, {});
+    const bash = spawn("sudo", ["-u", user ?? "dev", command, ...args], {
+      shell: true,
+      cwd,
+    });
 
     bash.stdout.on("data", (data) => {
       emitter.emit("data", data.toString().trim());
